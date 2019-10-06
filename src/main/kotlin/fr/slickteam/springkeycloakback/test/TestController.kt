f4df9efb195120806c7fb0eeca6a3e2765ae2d6e
Ronan Morel <ronan.morel@slickteam.fr>
2019-10-06 14:41:56 +0200

    Finition

--- src/main/kotlin/fr/slickteam/springkeycloakback/test/TestController.kt
+++ src/main/kotlin/fr/slickteam/springkeycloakback/test/TestController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:8443")
 class TestController {
 
     @GetMapping(value = ["", "/public"])
